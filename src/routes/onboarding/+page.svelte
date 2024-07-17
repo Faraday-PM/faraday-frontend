ae8b1cf6148a42a17a47d1e5b8bbaa925254cfca
Emilio Mendoza <ironislife39@gmail.com>
2024-07-17 16:58:27 -0300

    Fixed delay after sentence

--- src/routes/onboarding/+page.svelte
+++ src/routes/onboarding/+page.svelte
@@ -1,36 +1,49 @@
 <script lang="ts">
+  import sleep from "../../utility/sleep";
 
   let introWords = [
     "Welcome to Faraday",
     "Faraday is a platform that helps you to manage your passwords.",
-  ]
-  let i = 0
-  let j = 0
-  let text = introWords[0]
-  let text2 = introWords[1]
-  let speed = 50
-  let speed2 = 50
-  let typeWriter = () => {
-    if (i < text.length) {
-      text += text.charAt(i)
-      i++
-      setTimeout(typeWriter, speed)
+  ];
+  let i = 0;
+  let j = 0;
+  let text = "";
+  let currentWord = "";
+  let isDeleting = false;
+  let speed = 150;
+
+  let typeWriter = async () => {
+    currentWord = introWords[i];
+    if (isDeleting) {
+      text = currentWord.substring(0, j - 1);
+      j--;
+      if (j == 0) {
+        isDeleting = false;
+        i++;
+        if (i == introWords.length) {
+          i = 0;
+        }
+      }
     } else {
-      if (j < text2.length) {
-        text += text2.charAt(j)
-        j++
-        setTimeout(typeWriter, speed2)
+      text = currentWord.substring(0, j + 1);
+      j++;
+      if (j == currentWord.length) {
+        await sleep(1000);
+        isDeleting = true;
       }
     }
-  }
-  typeWriter()
-
-
+    setTimeout(typeWriter, speed);
+  };
+  typeWriter();
 </script>
 
 <div class="h-[600px] w-[375px] font-mono">
   <div class="flex items-center justify-center h-screen flex-col">
-    <p class="text-2xl pb-3 font-bold transition-opacity ease-in duration-700 opacity-100 hover:opacity-0">{text}</p>
+    <p
+      class="text-2xl pb-3 font-bold transition-opacity ease-in duration-700 opacity-100 hover:opacity-0"
+    >
+      {text}
+    </p>
     <!--
     <p class="text-sm px-4">Faraday is a platform that helps you to manage your passwords.</p>
     -->
@@ -58,6 +71,4 @@
   </div>
   
   -->
-
-
 </div>
