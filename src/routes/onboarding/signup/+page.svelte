0f16e37eedd1f5faca2fc80d71c2ef402996e4ad
Emilio Mendoza <ironislife39@gmail.com>
2024-07-22 17:00:23 -0300

    Added basic inputs

--- src/routes/onboarding/signup/+page.svelte
+++ src/routes/onboarding/signup/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  let show_password = true;
-
-  $: type = show_password ? "text" : "password";
 </script>
 
 <div class="h-[600px] w-[375px] font-mono">
@@ -9,7 +6,7 @@
     <div class="flex-row justify-center items-center">
       <h2 class="font-bold text-xl">Create Account</h2>
       <div class="form-control">
-        <label class="input input-bordered flex items-center gap-2">
+        <label class="input input-bordered flex items-center gap-2 mb-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
@@ -23,9 +20,13 @@
               d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
             />
           </svg>
-          <input type="text" class="grow" placeholder="Email" />
+          <input
+            type="text"
+            class="grow focus:outline-none"
+            placeholder="Email"
+          />
         </label>
-        <label class="input input-bordered flex items-center gap-2">
+        <label class="input input-bordered flex items-center gap-2 mb-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
@@ -36,44 +37,49 @@
               d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
             />
           </svg>
-          <input type="text" class="grow" placeholder="Username" />
+          <input
+            type="text"
+            class="grow focus:outline-none"
+            placeholder="Username"
+          />
         </label>
-        <label class="input input-bordered flex items-center gap-2">
-          {#if show_password}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-6 hover:cursor-pointer"
-              on:click={() => (show_password = !show_password)}
-              role="button"
-              tabindex="0"
-              on:keydown={(event) => {
-                if (event.key === "Enter" || event.key === " ") {
-                  show_password = !show_password;
-                }
-              }}
-            >
-          {:else}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-6"
-            >
-              <path
-                d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z"
-              />
-              <path
-                d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z"
-              />
-              <path
-                d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z"
-              />
-            </svg>
-          {/if}
-
-          <input type="password" placeholder="Password" />
+        <label class="input input-bordered flex items-center gap-2 mb-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="h-4 w-4 opacity-70"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <input
+            type="password"
+            placeholder="Password"
+            class="grow focus:outline-none"
+          />
+        </label>
+        <label class="input input-bordered flex items-center gap-2 mb-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="h-4 w-4 opacity-70"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            class="grow focus:outline-none"
+          />
         </label>
       </div>
     </div>
