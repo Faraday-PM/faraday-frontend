10b4dc2e807c55eb54d11903c6aa9af5ab1e7972
Emilio Mendoza <ironislife39@gmail.com>
2024-11-25 13:21:33 -0500

    Removed unused functions

--- static/scripts/autofill.js
+++ static/scripts/autofill.js
@@ -4,23 +4,6 @@
 //  }
 //});
 
-let fillable = false;
-
-function containsLogin() {
-  const keywords = ["password", "email", "username"];
-
-  const inputs = document.getElementsByTagName("input");
-  for (let i = 0; i < inputs.length; i++) {
-    console.log("HELLOO");
-    let type = inputs[i].type.toLowerCase();
-    let autocomplete = inputs[i].autocomplete.toLowerCase();
-    if (keywords.includes(type) || keywords.includes(autocomplete)) {
-      return true;
-    }
-  }
-  return false;
-}
-
 async function NodeListener() {
   const observer = new MutationObserver(async (mutations) => {
     const keywords = ["password", "email", "username"];
@@ -102,8 +85,6 @@ async function getCredentials() {
 }
 
 async function pageLoad() {
-  // JS errors suck
-
   const v = await chrome.storage.local.get(["fvault"]);
   const vault = v["fvault"]["vault"];
   const url = window.location.host;
@@ -133,34 +114,6 @@ async function pageLoad() {
 
     // type in email/username
     if (["email", "username"].indexOf(type) >= 0) {
-      input.value = username;
-    }
-    if (type == "password") {
-      input.value = password;
-    }
-  }
-  /* console.log("Running");
-  try {
-    const [username, password] = await getCredentials();
-    console.log(username);
-    autoFill(username, password);
-  } catch (e) {
-    console.log(e.message);
-    throw e;
-  }
-  */
-}
-
-function autoFill(username, password) {
-  const inputs = document.getElementsByTagName("input");
-  const inputLength = inputs.length;
-  for (let i = 0; i < inputLength; i++) {
-    const input = inputs.item(i);
-
-    const type = input.type.toLowerCase();
-    // Purpose?
-    // const autocomplete = input.autocomplete.toLowerCase();
-    if (type == "email" || type == "username") {
       input.value = username;
     }
     if (type == "password") {
